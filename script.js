bdc2a060fc0f71040135942f058b692543ce22fe
Rafid Hoda <rafidhoda@gmail.com>
2023-11-11 10:36:59 +0100

    firebase setup

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Yo yo yo")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcZ0oOMRS0D2DvkmAflK-F8PUvZqdJg_I",
+  authDomain: "add-to-cart---hoda-app.firebaseapp.com",
+  projectId: "add-to-cart---hoda-app",
+  storageBucket: "add-to-cart---hoda-app.appspot.com",
+}
+
+const app = initializeApp(firebaseConfig)
+
+console.log(app)
